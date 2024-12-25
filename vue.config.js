80bf844ee06c972a96aecd0b98bac34d1e618966
Nihil <35506793+Nihilnia@users.noreply.github.com>
2024-12-25 17:05:47 +0100

    _

--- vue.config.js
+++
@@ -1,5 +0,0 @@
-const { defineConfig } = require("@vue/cli-service");
-module.exports = defineConfig({
-  transpileDependencies: true,
-  publicPath: "/hmm/", // Replace <REPOSITORY_NAME> with your GitHub repository name
-});
